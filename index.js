fda04378600258c6759d27a5f567cc9fbe050dee
Tiago Alves <alvesjtiago@gmail.com>
2018-08-18 19:35:02 -0500

    First working version of cb.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const chalk     = require('chalk');
 const inquirer  = require('inquirer');
 const simpleGit = require('simple-git');
@@ -9,10 +10,28 @@ if (!files.directoryExists('.git')) {
   process.exit();
 }
 
-simpleGit().branchLocal()
-  .exec((err, branches) => {
-    if (!branches) {
+simpleGit()
+  .branchLocal(function (err, branchSummary) {
+    const branches = Object.keys(branchSummary.branches);
+    const numberOfBranches = branches.length
+    if (numberOfBranches == 0) {
       console.log(chalk.red('There are no git branches defined.'));
       process.exit();
     }
+
+    const questions = [
+      {
+        type: 'list',
+        name: 'branch',
+        message: 'Select the branch you want to switch to:',
+        choices: branches,
+        default: []
+      }
+    ];
+    
+    inquirer
+      .prompt(questions)
+      .then(answer => {
+        simpleGit().checkout(answer.branch);
+      });
   });
